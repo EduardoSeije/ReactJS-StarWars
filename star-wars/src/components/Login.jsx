16cf6cf4ed6f5f3e7b0d7aca0d91019b8113bbdb
Eduardo Seije <eduardoseije@gmail.com>
2021-09-09 09:55:13 -0300

    Iniciando a tela de login. Criação dos inputs e labels com estilos iniciais

--- star-wars/src/components/Login.jsx
+++ star-wars/src/components/Login.jsx
@@ -1,10 +1,19 @@
 // Página de Login  para acessar ao projeto 
 import React from 'react';
+import './login.css';
 
 class Login extends React.Component {
   render() {
     return (
-      <h1>Login</h1>
+      <div className='form_group'>
+        <h1>Login</h1>
+        <input type='name' name='name' id='name' placeholder='Name' required />
+        <label htmlFor='name' className='form_label'>Name</label>
+        <input type='email' name='email' id='email' placeholder='E-mail' required/> 
+        <label htmlFor='email' className='form_label'>E-mail</label>
+        <input type='password' name='password' id='password' placeholder='Password' />
+        <label htmlFor='password' className='form_label'>Password</label>
+      </div>
     ); 
   };
 };
